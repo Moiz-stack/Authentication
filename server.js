ea0c3392cae07e852d09b0d888438b39ad0de415
Moiz Arif <moiz.workmail@gmail.com>
2025-03-29 23:41:58 +0500

    add port

--- server.js
+++ server.js
@@ -17,3 +17,9 @@ mongoose.connect(process.env.MONGO_URI)
 ]);
 
 app.use('/api/auth',authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
